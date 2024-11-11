f1aa02ecc5e8d145d98bba51ddb0c44f88e88d7d
sabeehkc <wg457129@gmail.com>
2024-11-11 23:25:38 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+/*~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~*/
+const navMenu = document.getElementById('nav-menu')
+const navLink = document.querySelectorAll('.nav-link')
+const hambuger = document.getElementById('hamburger')
+
+hambuger.addEventListener("click",() => {
+    navMenu.classList.toggle('left-[0]')
+    hambuger.classList.toggle('ri-close-large-line')
+})
+
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.toggle('left-[0]')
+        hambuger.classList.toggle('ri-close-large-line')
+    })
+})
+
+
+/*~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~*/
+
+
+
+/*~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~*/
+
+/*~~~~~~~~~~ SWIPER ~~~~~~~~~~*/
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed:400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+
+    }
+});
